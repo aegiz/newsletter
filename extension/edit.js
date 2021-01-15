692b08f83c44d5b5d1ec95528bfbf8ae3eea2ae4
Adrien Rahier <adrien@circularise.com>
2021-01-15 18:22:00 +0100

    Apply misc changes on the newsletter body and code rules

--- extension/edit.js
+++ extension/edit.js
@@ -14,18 +14,13 @@ var blobUrl;
  ************************
  */
 
-function isRetina() {
-  return (
-    ((window.matchMedia &&
-      (window.matchMedia(
-        "only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx), only screen and (min-resolution: 75.6dpcm)"
-      ).matches ||
-        window.matchMedia(
-          "only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min--moz-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)"
-        ).matches)) ||
-      (window.devicePixelRatio && window.devicePixelRatio >= 2)) &&
-    /(iPad|iPhone|iPod)/g.test(navigator.userAgent)
-  );
+function isRetinaDisplay() {
+  if (window.matchMedia) {
+    var mq = window.matchMedia(
+      "only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)"
+    );
+    return (mq && mq.matches) || window.devicePixelRatio > 1;
+  }
 }
 
 function calculateReadingTime(words) {
@@ -40,7 +35,7 @@ function calculateReadingTime(words) {
  */
 
 var initialization = function (request) {
-  if (isRetina()) {
+  if (isRetinaDisplay()) {
     $("body").addClass("retina");
   }
   $("#base").css("backgroundImage", "url(" + backgroundImage + ")");
